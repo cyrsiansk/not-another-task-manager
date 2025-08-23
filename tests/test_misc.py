8ea9a16e5990156a15e0b39bf584a1276b2a3dc6
cyrsiansk <cyrsiadv@gmail.com>
2025-08-24 02:58:57 +0400

    Additional tests

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,5 +1,13 @@
-from app.main import health
+from starlette.testclient import TestClient
+
+from app.main import health, app
 
 
 def test_health_endpoint_function():
     assert health() == {"status": "ok"}
+
+
+def test_swagger_docs_endpoint():
+    endpoint = "/docs"
+    with TestClient(app) as client:
+        assert client.get(endpoint).status_code == 200
